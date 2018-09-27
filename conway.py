31e945dc4491b76b1366e016b0f240c235dd24ee
clee088 <42843045+clee088@users.noreply.github.com>
2018-09-27 11:57:17 -0400

    Updated from Brython Server: 9/27/2018 11:57:14 AM

--- conway.py
+++ conway.py
@@ -1,8 +1,9 @@
 """
 conway.py
-Author: <your name here>
+Author: Christopher Lee
 Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+print("I love dick")
