1953f3f65aa6efa5de2c087505da7219480b4b79
clee088 <42843045+clee088@users.noreply.github.com>
2018-10-01 17:41:22 -0400

    Updated from Brython Server: 10/1/2018 5:41:19 PM

--- conway.py
+++ conway.py
@@ -7,26 +7,26 @@ Write and submit a program that plays Conway's Game of Life, per
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import *
+#Colors and Lines
+black = Color(0, 0)
+white = Color(0xffffff, 1)
+grey = Color(0xC0C0C0, 1)
+noline = LineStyle(1, grey)
 
+
+#--
 class cell(Sprite):
-    def __init__(self):
-        super().__init__()
-        black = Color(0, 1)
-        white = Color(0xFFFF, 0)
-        grey = Color(0xC0C0C0, 1)
-        cell = RectangleAsset(100, 100, noline, white)
-        
+    def __init__(self, position):
+        cell = RectangleAsset(50, 50, noline, white)
+        cell = Sprite(cell)
 
 class map(App):
     def __init__(self):
         super().__init__()
-        black = Color(0, 1)
-        white = Color(0xFFFF, 0)
-        grey = Color(0xC0C0C0, 1)
-        noline = LineStyle(1, grey)
-        grid = RectangleAsset(500, 500, noline, white)
+        grid = RectangleAsset(500, 500, noline, black)
         grid = Sprite(grid, (0,0))
-        cell((0,0))
+        cell((0, 0))
+        
 
 
 myapp = map()
