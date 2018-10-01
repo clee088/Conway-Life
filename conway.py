78e1496b4027bd6d540e75f832791f6af53bde11
clee088 <42843045+clee088@users.noreply.github.com>
2018-10-01 18:11:11 -0400

    Updated from Brython Server: 10/1/2018 6:11:08 PM

--- conway.py
+++ conway.py
@@ -8,7 +8,7 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import *
 #Colors and Lines
-black = Color(0, 0)
+black = Color(0, 1)
 white = Color(0xffffff, 1)
 grey = Color(0xC0C0C0, 1)
 noline = LineStyle(1, grey)
@@ -17,17 +17,23 @@ noline = LineStyle(1, grey)
 #--
 class cell(Sprite):
     def __init__(self, position):
-        cell = RectangleAsset(50, 50, noline, white)
-        cell = Sprite(cell)
+        c = RectangleAsset(50, 50, noline, white)
+        super().__init__(c.asset, position)
+        c = Sprite(cell)
 
+
+#--
+
+
+
+
+#--
 class map(App):
     def __init__(self):
         super().__init__()
-        grid = RectangleAsset(500, 500, noline, black)
+        grid = RectangleAsset(500, 500, noline, white)
         grid = Sprite(grid, (0,0))
         cell((0, 0))
-        
-
 
 myapp = map()
 myapp.run()
